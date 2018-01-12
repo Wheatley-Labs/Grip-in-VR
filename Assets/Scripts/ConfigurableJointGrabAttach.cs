03e7ca92737e54a599902faf846ac065dac5d695
Unknown <michael@bonfert-online.de>
2018-01-12 13:33:21 +0100

    Add Configurable Joint Grab Attach for grabbing a dangling object

---
+++ Assets/Scripts/ConfigurableJointGrabAttach.cs
@@ -0,0 +1,48 @@
+﻿// Configurable Joint Grab Attach|GrabAttachMechanics|50040
+// Adapted from Fixed Joint Grab Attach by Michael Bonfert
+namespace VRTK.GrabAttachMechanics
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The Hinge Joint Grab Attach script is used to create a Configurable Joint connection between the object and the grabbing object.
+    /// </summary>
+    
+    [AddComponentMenu("Scripts/ConfigurableJointGrabAttach")]
+    public class ConfigurableJointGrabAttach : VRTK_BaseJointGrabAttach
+    {
+        [Tooltip("Maximum force the joint can withstand before breaking. Infinity means unbreakable.")]
+        public float breakForce = Mathf.Infinity;
+        [Tooltip("Whether the motion around the x axis is free, limited or locked.")]
+        public ConfigurableJointMotion xMotion = ConfigurableJointMotion.Free;
+        [Tooltip("Whether the motion around the y axis is free, limited or locked.")]
+        public ConfigurableJointMotion yMotion = ConfigurableJointMotion.Free;
+        [Tooltip("Whether the motion around the z axis is free, limited or locked.")]
+        public ConfigurableJointMotion zMotion = ConfigurableJointMotion.Free;
+        [Tooltip("Should the connectedAnchor be calculated automatically?")]
+        public bool autoConfigureConnectedAnchor = false;
+        [Tooltip("The Vector3 position of the connected anchor.")]
+        public Vector3 connectedAnchor = new Vector3(0f, -0.06f, 0.05f);
+
+        protected override void CreateJoint(GameObject obj)
+        {
+            givenJoint = obj.AddComponent<ConfigurableJoint>();
+            givenJoint.breakForce = (grabbedObjectScript.IsDroppable() ? breakForce : Mathf.Infinity);
+            givenJoint.autoConfigureConnectedAnchor = autoConfigureConnectedAnchor;
+            givenJoint.connectedAnchor = connectedAnchor;
+            base.CreateJoint(obj);
+            ConfigureJoint();
+        }
+
+        void ConfigureJoint()
+        {
+            ConfigurableJoint thisJoint = gameObject.GetComponent<ConfigurableJoint>();
+            thisJoint.xMotion = xMotion;
+            thisJoint.yMotion = yMotion;
+            thisJoint.zMotion = zMotion;
+
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/HingeJointGrabAttach.cs
@@ -0,0 +1,24 @@
+﻿// Hinge Joint Grab Attach|GrabAttachMechanics|50040
+// Adapted from Fixed Joint Grab Attach by Michael Bonfert
+namespace VRTK.GrabAttachMechanics
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The Hinge Joint Grab Attach script is used to create a Hinge Joint connection between the object and the grabbing object.
+    /// </summary>
+    
+    [AddComponentMenu("Scripts/HingeJointGrabAttach")]
+    public class HingeJointGrabAttach : VRTK_BaseJointGrabAttach
+    {
+        [Tooltip("Maximum force the joint can withstand before breaking. Infinity means unbreakable.")]
+        public float breakForce = Mathf.Infinity;
+
+        protected override void CreateJoint(GameObject obj)
+        {
+            givenJoint = obj.AddComponent<HingeJoint>();
+            givenJoint.breakForce = (grabbedObjectScript.IsDroppable() ? breakForce : Mathf.Infinity);
+            base.CreateJoint(obj);
+        }
+    }
+}
