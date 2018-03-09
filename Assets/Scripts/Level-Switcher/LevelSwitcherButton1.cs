9565d4347250422757c108911d107f584ddc5d9b
Unknown <michael@bonfert-online.de>
2018-03-09 17:24:57 +0100

    Add Level Switcher; Create Scenes for each of 5 modes; Adjust scene 1

---
+++ Assets/Scripts/Level-Switcher/LevelSwitcherButton1.cs
@@ -0,0 +1,28 @@
+﻿namespace VRTK.Examples
+{
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class LevelSwitcherButton1 : VRTK_InteractableObject
+    {
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            StartCoroutine(LoadAsyncScene());
+        }
+
+        IEnumerator LoadAsyncScene()
+        {
+            AsyncOperation loadState = SceneManager.LoadSceneAsync("Binary-Grip_Pretest_1_Baseline");
+
+            while (!loadState.isDone)
+            {
+                yield return null;
+            }
+        }
+    }
+
+}
