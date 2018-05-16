80884ea60a84c1ad59dfbe694bc26322affeac66
Michael <michael@bonfert-online.de>
2018-05-16 18:14:57 +0200

    Create target areas for Pokal-Glasses with animations

---
+++ Assets/Scripts/Main Study Tasks/PlaceTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTarget : MonoBehaviour {
+    Animator targetAnimator;
+
+	// Use this for initialization
+	void Start () {
+        targetAnimator = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pokal"))
+        {
+            targetAnimator.SetBool("success", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pokal"))
+        {
+            targetAnimator.SetBool("success", false);
+        }
+    }
+}
