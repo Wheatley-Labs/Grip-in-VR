152711e8d09f75822c68747e43c93f8aa208370c
Michael <michael@bonfert-online.de>
2018-05-16 18:14:12 +0200

    Make Pokal-Glass blue, adjust center of mass

---
+++ Assets/Scripts/AccessCenterOfMass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessCenterOfMass : MonoBehaviour {
+
+    public GameObject centerOfMass;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = transform.InverseTransformPoint(centerOfMass.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
