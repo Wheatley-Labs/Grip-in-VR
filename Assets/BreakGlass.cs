3791b6ecdc577bdf9332ec510b5696c230580d35
Michael <michael@bonfert-online.de>
2018-05-18 17:26:15 +0200

    First attempt to break glass

---
+++ Assets/BreakGlass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakGlass : MonoBehaviour {
+    public GameObject window;
+    public float stability;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > stability)
+            Break();
+    }
+
+    private void Break()
+    {
+        GameObject windowInstance;
+        windowInstance = Instantiate(window, gameObject.transform);
+        //windowInstance.GetComponent<BreakableWindow>().breakWindow();
+        Destroy(gameObject);
+    }
+}
+
