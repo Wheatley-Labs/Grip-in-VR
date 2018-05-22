b31aff4bc85a9cd0b4a7403de09bf4b8e55c3a67
Michael <michael@bonfert-online.de>
2018-05-22 15:05:13 +0200

    Start implementing InteractionModeManager

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour {
+
+    static public int currentInteractionMode;
+    
+    public void SetMode()
+    {
+        GameObject[] grabbables;
+
+        grabbables = GameObject.FindGameObjectsWithTag("Grabbable");
+
+        foreach (GameObject obj in grabbables)
+        {
+            //check for current components
+            //adjust according to current mode
+        }
+    }
+}
