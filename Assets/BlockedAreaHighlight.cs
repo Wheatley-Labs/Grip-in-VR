10a6bc60eeb0eba003c901160af4fb18e60f916e
Michael <michael@bonfert-online.de>
2018-05-23 15:19:20 +0200

    Blocked Areas are highlighted when objects are close

---
+++ Assets/BlockedAreaHighlight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockedAreaHighlight : MonoBehaviour {
+    MeshRenderer meshRend;
+
+    private void Start()
+    {
+        meshRend = GetComponent<MeshRenderer>();
+        meshRend.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            meshRend.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            meshRend.enabled = false;
+        }
+    }
+}
