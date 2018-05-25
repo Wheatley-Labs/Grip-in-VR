06dc1e7a28e8fdec6545c992037df616c39353b4
Michael <michael@bonfert-online.de>
2018-05-25 15:51:09 +0200

    Drawer stays open and closed by constant force

---
+++ Assets/Scripts/Main Study Tasks/DrawerStaysOpenOrClosed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerStaysOpenOrClosed : MonoBehaviour {
+
+    private ConstantForce force;
+
+	// Use this for initialization
+	void Start () {
+        force = GetComponent<ConstantForce>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.localPosition.y < -4.6f)
+            force.force = Vector3.right;
+
+        else if (gameObject.transform.localPosition.y > -3.9f)
+            force.force = Vector3.left;
+
+        else
+            force.force = Vector3.zero;
+	}
+}
