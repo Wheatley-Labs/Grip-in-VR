24b3fee3b8627c2f56a3993d447e64e8dda49329
Michael <michael@bonfert-online.de>
2018-05-29 12:43:32 +0200

    Oven can now be opened

---
+++ Assets/Scripts/Main Study Tasks/OvenStaysOpenOrClosed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvenStaysOpenOrClosed : MonoBehaviour {
+
+    private ConstantForce force;
+
+	// Use this for initialization
+	void Start () {
+        force = GetComponent<ConstantForce>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.localEulerAngles.x > 330)
+            force.torque = Vector3.back /2;
+
+        else if (gameObject.transform.localEulerAngles.x < 280)
+            force.torque = Vector3.forward /2;
+
+        else
+            force.torque = Vector3.zero;
+	}
+}
