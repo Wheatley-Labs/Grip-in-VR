c8bd244aabbcf4fc2d95ad1f93bb6ecb5a1c3d6f
Michael <michael@bonfert-online.de>
2018-05-31 15:27:49 +0200

    Start Tutorials; Add Tooltip Continue button; highlight trigger

---
+++ Assets/Scripts/UI/Tutorials/Tutorial_Tooltip3_Grab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Tutorial_Tooltip3_Grab : MonoBehaviour {
+    public GameObject nextTooltip;
+    public VRTK_InteractableObject glass;
+    public GameObject ContrL;
+    public GameObject ContrR;
+
+	// Use this for initialization
+	void Start () {
+        ContrL.GetComponent<VRTK_ControllerHighlighter>().HighlightElement(SDK_BaseController.ControllerElements.Trigger, Color.cyan, 1f);
+        ContrR.GetComponent<VRTK_ControllerHighlighter>().HighlightElement(SDK_BaseController.ControllerElements.Trigger, Color.cyan, 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (glass.IsGrabbed())
+        {
+            //nextTooltip.SetActive(true);
+            ContrL.GetComponent<VRTK_ControllerHighlighter>().UnhighlightElement(SDK_BaseController.ControllerElements.Trigger);
+            ContrR.GetComponent<VRTK_ControllerHighlighter>().UnhighlightElement(SDK_BaseController.ControllerElements.Trigger);
+            Destroy(gameObject);
+        }
+	}
+}
