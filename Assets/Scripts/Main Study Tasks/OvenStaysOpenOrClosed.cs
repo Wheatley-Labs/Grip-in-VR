6fef37a2ae4575c20b353f123f9ec16356e3df9f
Michael <michael@bonfert-online.de>
2018-06-12 11:48:51 +0200

    Replace carafes with glasses; Fix oven - again

--- Assets/Scripts/Main Study Tasks/OvenStaysOpenOrClosed.cs
+++ Assets/Scripts/Main Study Tasks/OvenStaysOpenOrClosed.cs
@@ -1,37 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class OvenStaysOpenOrClosed : MonoBehaviour {
 
     private ConstantForce force;
+    private VRTK_InteractableObject interactableObject;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         force = GetComponent<ConstantForce>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (gameObject.transform.localEulerAngles.x > 320)
-            force.torque = Vector3.back /2;
+            force.torque = Vector3.back / 2;
 
         else if (gameObject.transform.localEulerAngles.x < 290)
-            force.torque = Vector3.forward /2;
+            force.torque = Vector3.forward / 2;
 
         else
             force.torque = Vector3.zero;
 
-        //if (gameObject.transform.localEulerAngles.x > 358 && gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.1)
-        //{
-
+        if (gameObject.transform.localEulerAngles.x > 358 && gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.1 && !interactableObject.IsGrabbed())
+        {
+            StartCoroutine(MoveAway());
+        }
+    }
+        
     IEnumerator MoveAway()
     {
         yield return new WaitForSeconds(0.3f);
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX;
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
         Destroy(GetComponent<HingeJoint>());
-        foreach(Collider col in GetComponents<Collider>())
+        foreach (Collider col in GetComponents<Collider>())
         {
             Destroy(col);
         }
